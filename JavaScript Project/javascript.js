898a737c23f1d98ba06be885032e4b1584c45010
Joebidenlover1 <9023993@student.zadkine.nl>
2024-01-25 09:15:46 +0100

    Hey SEXY LADIES 10 KM + CLICK THE LINK NO SCAM!!!

--- JavaScript Project/javascript.js
+++ JavaScript Project/javascript.js
@@ -1,13 +1,16 @@
-var myDate = new Date();
-var hrs = myDate.getHours();
-
-var greet;
-
-if (hrs < 12)
-    greet = 'Goede Morgen';
-else if (hrs >= 12 && hrs <= 17)
-    greet = 'Goede Middag';
-else if (hrs >= 17 && hrs <= 24)
-    greet = 'Fijne Avond';
-
-document.getElementById('greetings').innerHTML ='<b>' + greet + '</b>';
+function talk(){
+    var know = {
+        "Hoe gaat het?" : "Goed :))",
+        "Aanmelden" : " Ga naar https://www.techniekcollegerotterdam.nl/aanmelden om je te aanmelden :)",
+        "Wat voor school is TCR?" : "Wij zijn Techniek College Rotterdam. De grootste mbo-school voor technisch beroepsonderwijs in Rotterdam-Rijnmond. Bij ons vind je technische mbo-opleidingen, cursussen en trainingen voor jongeren én volwassenen. ",
+        "Locatie" : "PRINS ALEXANDERLAAN 55 + 3067 GB ROTTERDAM",
+        "Wanneer is de open dag?" : " Kom langs op 26 januari van 15.00 tot 20.00 uur of op 27 januari van 10.00 tot 14.00 uur",
+    };
+    var user = document.getElementById('userBox').value;
+    document.getElementById('chatLog').innerHTML = user + "<br>";
+    if (user in know) {
+        document.getElementById('chatLog').innerHTML = know[user] + "<br>";
+    }else{
+        document.getElementById('chatLog').innerHTML = "Sorry, ik begrijp je vraag niet :( <br>";
+    }
+}
